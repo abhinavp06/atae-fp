78fff05fbb1a39b3398a566f834f8f2a72aa576a
abhinavp06 <aparasharchd06@gmail.com>
2026-07-26 16:01:40 +0530

    audio buffer struct instead of class

--- CORE/include/atae_fp/types/audio_buffer.h
+++ CORE/include/atae_fp/types/audio_buffer.h
@@ -1,14 +1,7 @@
 #pragma once
 #include <vector>
 
-class AudioBuffer {
-public:
-	void setSampleRate(double sample_rate);
-	void setDuration(double duration_s);
-	void reset();
-private:
-	double sample_rate_ = 44100.0;
-	double duration_s_ = 0.0;
-protected:
-	std::vector<double> buffer_;
+struct AudioBuffer {
+	std::vector<double> samples;
+	double sample_rate;
 };
