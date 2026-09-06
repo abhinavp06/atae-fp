1e19c80b020352efdfadab9a38b5e55614518c9a
abhinavp06 <aparasharchd06@gmail.com>
2026-09-06 17:35:25 +0530

    pink noise header file

---
+++ CORE/include/atae_fp/noise/pink_noise.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <atae_fp/types/audio_buffer.h>
+
+class PinkNoise {
+public:
+	void setSampleRate(double sample_rate);
+	AudioBuffer generate(const double duration_s);
+private:
+	const int NUM_ROWS = 20;
+	double sample_rate_ = 44100.0;
+};
