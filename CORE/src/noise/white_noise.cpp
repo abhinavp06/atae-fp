689017bd6900852c523f8f608120e1615291abd2
abhinavp06 <aparasharchd06@gmail.com>
2026-09-08 08:36:00 +0200

    added process sample function

--- CORE/src/noise/white_noise.cpp
+++ CORE/src/noise/white_noise.cpp
@@ -4,15 +4,18 @@ void WhiteNoise::setSampleRate(double sample_rate) {
 	sample_rate_ = sample_rate;
 }
 
+double WhiteNoise::processSample() {
+	return dist_(engine_);
+}
+
 AudioBuffer WhiteNoise::generate(const double duration_s) {
 	int sample_count = static_cast<int>(sample_rate_ * duration_s);
 	
 	buffer_.samples.clear();
 	buffer_.samples.reserve(sample_count);
 
-
 	for (int i = 0; i < sample_count; i++) {
-		buffer_.samples.push_back(dist_(engine_));
+		buffer_.samples.push_back(processSample());
 	}
 
 	return buffer_;
