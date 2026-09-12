0d471396ea76d71328cc8c1a2dd51b6645811d12
abhinavp06 <aparasharchd06@gmail.com>
2026-09-13 00:31:41 +0530

    added logs

--- CORE/src/noise/pink_noise.cpp
+++ CORE/src/noise/pink_noise.cpp
@@ -15,4 +15,21 @@ AudioBuffer PinkNoise::generate(const double duration_s) {
 	* 2. running_sum for better operations
 	* 3. each row is an emitter...
 	*/
+
+	/**
+	* rows[0] = "* * * * * * *"
+	* rows[1] = "  *   *   *  "
+	* rows[2] = "      *      "
+	* 
+	* octave = 2^k
+	* so from 20 to 20,000 how many octaves?
+	* lower octaves to have "more" energy compared to the higher ones because of density.. lower octaves have less width in terms of frequency, hence more energy per "point" (upto a certain precision)
+	* 
+	* so either fill all rows first and then remove.. or fill rows for a sample and then push that to the buffer?
+	* latter is better
+	* 
+	* for(  i < total_samples )
+	*	1. find out which rows need to be added
+	*	2. add those rows, get a value, normalize it by the number of rows, save to buffer
+	*/
 }
