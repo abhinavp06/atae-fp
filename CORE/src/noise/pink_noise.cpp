0220eb7424a7fd7bc6e9dde724932e87cd79ae2c
abhinavp06 <aparasharchd06@gmail.com>
2026-09-12 23:35:38 +0530

    pink_noise implementation plan

--- CORE/src/noise/pink_noise.cpp
+++ CORE/src/noise/pink_noise.cpp
@@ -6,5 +6,13 @@ void PinkNoise::setSampleRate(double sample_rate) {
 }
 
 AudioBuffer PinkNoise::generate(const double duration_s) {
-	
+	std::vector<double> rows(NUM_ROWS, 0.0);
+
+	double running_sum = 0.0;
+
+	/**
+	* 1. emit white noise at different frequencies... 2^k
+	* 2. running_sum for better operations
+	* 3. each row is an emitter...
+	*/
 }
