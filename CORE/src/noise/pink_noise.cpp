d5a6126ec2a966ca01ee5362ca2c44b19130e110
abhinavp06 <aparasharchd06@gmail.com>
2026-09-13 00:55:02 +0530

    pink noise naive implementation

--- CORE/src/noise/pink_noise.cpp
+++ CORE/src/noise/pink_noise.cpp
@@ -1,4 +1,5 @@
 #include <atae_fp/noise/pink_noise.h>
+#include <cmath>
 
 void PinkNoise::setSampleRate(double sample_rate) {
 	sample_rate_ = sample_rate;
@@ -6,6 +7,12 @@ void PinkNoise::setSampleRate(double sample_rate) {
 }
 
 AudioBuffer PinkNoise::generate(const double duration_s) {
+	int sample_count = static_cast<int>(sample_rate_ * duration_s);
+
+	AudioBuffer result_buffer;
+	result_buffer.samples.clear();
+	result_buffer.samples.reserve(sample_count);
+
 	std::vector<double> rows(NUM_ROWS, 0.0);
 
 	double running_sum = 0.0;
@@ -32,4 +39,37 @@ AudioBuffer PinkNoise::generate(const double duration_s) {
 	*	1. find out which rows need to be added
 	*	2. add those rows, get a value, normalize it by the number of rows, save to buffer
 	*/
+
+	// filling up rows initially with an "energy" value
+	for (int i = 0; i < NUM_ROWS; i++) {
+		rows[i] = white_noise_.processSample();
+	}
+
+	// iterate over samples
+	for (int i = 0; i < sample_count; i++) {
+		// I have rows 0 to NUM_ROWS
+		// find highest power of 2 which can contain the current value of i
+		// that gives you the highest index till which rows should be added up to
+		// add all row values till that index, normalize and push
+
+		// naive loop to check for highest power of two
+		int j = 0;
+		while ((std::pow(2, j)) < i) {
+			j++;
+		}
+
+		// now loop through all rows up until that index
+		double row_sum = 0.0;
+		for (int k = 0; k < j; k++) {
+			row_sum += rows[k];
+		}
+
+		// normalize this sum
+		// normalize by NUM_ROWS or j?
+		row_sum /= NUM_ROWS;
+
+		result_buffer.samples.push_back(row_sum);
+	}
+
+	return result_buffer;
 }
